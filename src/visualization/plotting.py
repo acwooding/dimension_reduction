0b565afb312473a67f012ff0e3e091a8b3931faf
Amy Wooding <amy@wooding.org>
2018-07-12 11:45:57 -0400

    return fig with ax, whitespace

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -3,7 +3,6 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import base64
-import bokeh
 
 import math
 
@@ -55,7 +54,10 @@ def two_dim_multiplot(data, labels_list, titles, ncols=2,
                               s=s, **kwargs)
         plt.title(titles[i])
 
-def plot_3d_dataset(data, color_data, title='3d plot', figsize=(8,8), dim_list=None, cmap=None, **kwargs):
+
+def plot_3d_dataset(data, color_data, title='3d plot',
+                    figsize=(8,8), dim_list=None,
+                    cmap=None, **kwargs):
     '''Display a basic (colored) 3d plot
     dim_list:
         indices to use for each of the 3 dimensions of the plot
@@ -74,11 +76,13 @@ def plot_3d_dataset(data, color_data, title='3d plot', figsize=(8,8), dim_list=N
 
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(data[:, dim_list[0]], data[:, dim_list[1]], data[:, dim_list[2]],
-             c=color_data, cmap=cmap)
+    ax.scatter(data[:, dim_list[0]], data[:, dim_list[1]],
+               data[:, dim_list[2]],
+               c=color_data, cmap=cmap)
     ax.view_init(10)
     plt.title(title)
-    return ax
+    return fig, ax
+
 
 def sphere_plot(data, color_data, wireframe=False, title='sphere plot',
                 s=50, zorder=10, dim_list=None, cmap=None,
@@ -98,11 +102,13 @@ def sphere_plot(data, color_data, wireframe=False, title='sphere plot',
         x = np.outer(np.sin(theta), np.cos(phi))
         y = np.outer(np.sin(theta), np.sin(phi))
         z = np.outer(np.cos(theta), np.ones_like(phi))
-        ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1, linewidth=1)
-    ax.scatter(data[:, dim_list[0]], data[:, dim_list[1]], data[:, dim_list[2]],
-             c=color_data, cmap=cmap, s=s, zorder=zorder, **kwargs)
+        ax.plot_wireframe(x, y, z, color='k', rstride=1,
+                          cstride=1, linewidth=1)
+    ax.scatter(data[:, dim_list[0]], data[:, dim_list[1]],
+               data[:, dim_list[2]], c=color_data,
+               cmap=cmap, s=s, zorder=zorder, **kwargs)
     plt.title(title)
-    return ax
+    return fig, ax
 
 
 def embeddable_image(filename):
@@ -114,4 +120,3 @@ def embeddable_image(filename):
     image.save(buffer, format='png')
     for_encoding = buffer.getvalue()
     return 'data:image/png;base64,' + base64.b64encode(for_encoding).decode()
-
